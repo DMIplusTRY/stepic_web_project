0336404714be2153e4614844a9d5a3f22152519e
DMIplusTRY <krotikov102@mail.ru>
2022-02-22 21:54:47 +0300

    new nginx.conf location no my

--- hello.py
+++ hello.py
@@ -1,8 +1,6 @@
-def wsgi_application(environ, start_response):
-    status = "200 OK"
-    headers = [("Content-Type", "text/plain")]
-    body = str()
-    for i in environ['QUERY_STRING'].split("&"):
-    	body += str(i) +"\n"
-    start_response(status, headers)
-    return [body.encode()]
+def wsgi_app(environ, start_response):
+    start_response("200 OK", [("Content-Type", "text/plain")])
+    res = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    return res
+
+bind = '0.0.0.0:8080'
--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-sudo ln -sf /home/box//web/ /home/box/
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx reload
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo gunicorn -c hello.py hello:wsgi_app
